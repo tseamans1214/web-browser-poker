c5325a69c6d674629450c9a37b83c83fb791feae
Tommy Seamans <tlseamans1214@gmail.com>
2022-06-29 17:05:54 -0700

    Add player functions, combined changes to document

--- js/player.js
+++ js/player.js
@@ -1,23 +1,55 @@
 class Player {
-    constructor(name, money) {
+    constructor(name, money, playerNumber) {
+        // Set properties
         this.name = name;
         this.money = money;
+        this.playerNumber = playerNumber;
         this.isFolded = false;
         this.roundBet = 0;
         this.cards = [];
+
+        // Set html elements
+        document.getElementById(`p${this.playerNumber}-name`).textContent = this.name;
+        document.getElementById(`p${this.playerNumber}-money`).textContent = '$' + this.money;
+        if (this.playerNumber === 0) {
+            document.getElementById("p0-amount").value = 0;
+        }
+    }
+    setCards(cards) {
+        this.cards = cards;
+        document.getElementById(`p${this.playerNumber}-c1`).src = this.cards[0].image;
+        document.getElementById(`p${this.playerNumber}-c2`).src = this.cards[1].image;
     }
     bet(betAmount) {
         this.money = this.money - betAmount;
+        this.roundBet = this.roundBet + betAmount;
+        document.getElementById(`p${this.playerNumber}-money`).textContent = '$' + this.money;
+        logGame(`${this.name} bets $${betAmount}!`);
+        return betAmount;
+    }
+    subtractMoney(amount) {
+        this.money = this.money - amount;
+        document.getElementById(`p${this.playerNumber}-money`).textContent = '$' + this.money;
+        return amount;
     }
     fold() {
         this.isFolded = true;
         cards = [];
+        document.getElementById(`p${this.playerNumber}-c1`).src = "card-back.png";
+        document.getElementById(`p${this.playerNumber}-c2`).src = "card-back.png";
+    }
+    resetPlayer() {
+        this.isFolded = false;
+        this.roundBet = 0;
+        this.cards = [];
+        document.getElementById(`p${this.playerNumber}-c1`).src = "card-back.png";
+        document.getElementById(`p${this.playerNumber}-c2`).src = "card-back.png";
     }
 }
 
 class CPU extends Player {
-    constructor(name, money, difficulty) {
-        super(name, money);
+    constructor(name, money, playerNumber, difficulty) {
+        super(name, money, playerNumber);
         this.difficulty = difficulty;
     }
     choice() {
