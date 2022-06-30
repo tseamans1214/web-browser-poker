bd8001a4c774221cf4e7cd9f1fa60d4590b329d8
Tommy Seamans <tlseamans1214@gmail.com>
2022-06-29 17:07:00 -0700

    Add game states and reset

--- js/main.js
+++ js/main.js
@@ -2,43 +2,97 @@ var deck = new Deck();
 var table;
 var player1;
 var player2;
+var round;
+var player1Turn = false;
 async function startGame() {
     document.getElementById("game-menu-background").style.display = "none";
     
     deck.shuffle();
-    player1 = new Player("Tommy", 2000);
-    player2 = new CPU("CPU1", 2000, "easy");
+    player1 = new Player("Tommy", 2000, 0);
+    player2 = new CPU("CPU1", 2000, 1, "easy");
     table = new Table(100, [player1, player2]);
     var cards =  await deck.drawCards(4);
-    player1.cards = new Array(cards[0], cards[1]);
-    player2.cards = [cards[2], cards[3]];
-    document.getElementById("p1-name").textContent = player1.name;
-    document.getElementById("p1-money").textContent = '$' + player1.money;
-    console.log(player1.cards);
-    document.querySelector("#p1-c1").src = player1.cards[0].image;
-    document.querySelector("#p1-c2").src = player1.cards[1].image;
-    document.getElementById("p1-amount").value = 0;
-    document.getElementById("p2-name").textContent = player2.name;
-    document.getElementById("p2-money").textContent = '$' + player2.money;
+    player1.setCards([cards[0], cards[1]]);
+    player2.setCards([cards[2], cards[3]]);
+    round = 1;
     logGame("Welcome to Web Browser Poker! Enjoy you match!");
-    document.getElementById("ante").textContent = '$' + table.ante;
-    document.getElementById("round-bet").textContent = '$' + 0;
-    logGame(`The ante for this round is $${table.ante}`);
+    table.collectAnte();
     logGame(`It is ${player1.name}'s turn, How much will you bet or will you fold`);
 }
 
-function p1Bet() {
-    var betAmount = Number(document.getElementById("p1-amount").value);
+function main() {
+    // while (true) {
+    //     if (round == 1) {
+    //         do {
+    //             for (var i=0; table.players.length; i++) {
+    //                 logGame(`It is ${table.players[i].name}'s turn, How much will you bet or will you fold`);
+    //                 if (i === 0) {
+    //                     player1Turn = true;
+    //                 }
+    //                 //while (player1Turn === true);
+    //                 if (i !== 0) {
+    //                     table.players[i].bet(10);
+    //                 }
+    //             }
+    //         } while (table.checkPlayersRoundBet() === false);
+    //     }
+    // }
+}
+
+async function p0Bet() {
+    var betAmount = Number(document.getElementById("p0-amount").value);
     player1.bet(betAmount);
-    document.getElementById("p1-money").textContent = '$' + player1.money;
-    logGame(`Player 1 bets $${betAmount}!`);
+    for (var i=1; i<table.players.length; i++) {
+        logGame(`It is ${table.players[i].name}'s turn, How much will you bet or will you fold`);
+        table.players[i].bet(betAmount);
+    }
+    if (table.checkPlayersRoundBet()) {
+        round++;
+    }
+    var cards;
+    if (round === 2) {
+        cards = await deck.drawCards(3);
+        table.addCards(cards);
+    } else if (round === 3 || round === 4) {
+        cards = await deck.drawCards(1);
+        table.addCards(cards);
+    } else {
+        table.calculateWinner();
+        resetRound();
+    }
+    logGame("Round: " + round);
+    logGame(`It is ${player1.name}'s turn, How much will you bet or will you fold`);
 }
 function logGame(message) {
     const newLog = document.createElement("p"); 
     newLog.textContent = message;
     document.getElementById("sec-game-log").appendChild(newLog);
+    document.getElementById('sec-game-log').scrollTo(0,100000);
 
 }
+function changeButtonText() {
+    console.log("Round Bet: " + table.roundBet);
+    console.log("Amount Bet: " + document.getElementById("p0-amount").value);
+    if (Number(document.getElementById("p0-amount").value) === Number(table.roundBet)) {
+        console.log("Check");
+        document.getElementById("bet").value = "Check";
+    } else {
+        console.log("Raise");
+        document.getElementById("bet").value = "Raise";
+    }
+}
+
+async function resetRound() {
+    round = 1;
+    document.getElementById('sec-game-log').innerHTML = "";
+    table.resetTable();
+    deck.shuffle();
+    var cards =  await deck.drawCards(4);
+    player1.setCards([cards[0], cards[1]]);
+    player2.setCards([cards[2], cards[3]]);
+
+}
+
 
 
 
