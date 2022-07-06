d2876d44785abee1de8babce41effde9f9667e06
Tommy Seamans <tlseamans1214@gmail.com>
2022-07-05 22:49:05 -0700

    add check bets, cpu ai, delay for logGame

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ var player1;
 var player2;
 var round;
 var player1Turn = false;
+var cpuRaise = false;
 async function startGame() {
+    document.getElementById("bet").disabled = true;
+    document.getElementById("fold").disabled = true;
     document.getElementById("game-menu-background").style.display = "none";
-    
     deck.shuffle();
     player1 = new Player("Tommy", 2000, 0);
     player2 = new CPU("CPU1", 2000, 1, "easy");
@@ -15,9 +17,11 @@ async function startGame() {
     player1.setCards([cards[0], cards[1]]);
     player2.setCards([cards[2], cards[3]]);
     round = 1;
-    logGame("Welcome to Web Browser Poker! Enjoy you match!");
-    table.collectAnte();
-    logGame(`It is ${player1.name}'s turn, How much will you bet or will you fold`);
+    await logGame("Welcome to Web Browser Poker! Enjoy you match!");
+    await table.collectAnte();
+    await logGame(`It is ${player1.name}'s turn, How much will you bet or will you fold`);
+    document.getElementById("bet").disabled = false;
+    document.getElementById("fold").disabled = false;
 }
 
 function main() {
@@ -40,59 +44,104 @@ function main() {
 }
 
 async function p0Bet() {
-    var betAmount = Number(document.getElementById("p0-amount").value);
-    player1.bet(betAmount);
-    table.setRoundBet(betAmount);
-    table.increasePot(betAmount);
-    for (var i=1; i<table.players.length; i++) {
-        logGame(`It is ${table.players[i].name}'s turn. How much will you bet or will you fold`);
-        table.players[i].bet(betAmount);
-        table.increasePot(betAmount);
+    document.getElementById("bet").disabled = true;
+    document.getElementById("fold").disabled = true;
+    var p0BetAmount = Number(document.getElementById("p0-amount").value);
+    if (p0BetAmount < 0) {
+        alert(`Your bet may not be less than 0!`);
+        return;
+    }
+    if (p0BetAmount + player1.roundBet < table.roundBet) {
+        alert(`You must bet at least $${table.roundBet-player1.roundBet} to continue!`);
+        return;
+    }
+    await player1.bet(p0BetAmount);
+    table.increasePot(p0BetAmount);
+    let minBet = 0;
+    if (player1.roundBet >= table.roundBet) {
+        if (player1.roundBet == table.roundBet && cpuRaise) {
+
+        } else {
+            table.setRoundBet(player1.roundBet);
+            cpuRaise = false;
+            for (var i=1; i<table.players.length; i++) {
+                await logGame(`It is ${table.players[i].name}'s turn. How much will you bet or will you fold`);
+                var cpuBet = await table.players[i].choice(table.roundBet-table.players[i].roundBet);
+                if (table.players[i].isFolded == false) {
+                    table.increasePot(cpuBet);
+                    if (cpuBet > table.roundBet) {
+                        table.setRoundBet(cpuBet);
+                        cpuRaise = true;
+                    }
+                }
+            }
+        }
+    }
+    if (table.checkPlayersPlaying() == false) {
+        handleWinner(player1, "fold");
+        return;
     }
     if (table.checkPlayersRoundBet()) {
         round++;
-    }
-    var cards;
-    if (round === 2) {
-        cards = await deck.drawCards(3);
-        table.addCards(cards);
-    } else if (round === 3 || round === 4) {
-        cards = await deck.drawCards(1);
-        table.addCards(cards);
+        for (let i=0; i<table.players.length; i++) {
+            table.players[i].roundBet = 0;
+        }
+        table.setRoundBet(0);
+        cpuRaise = false;
+        var cards;
+        if (round === 2) {
+            cards = await deck.drawCards(3);
+            table.addCards(cards);
+        } else if (round === 3 || round === 4) {
+            cards = await deck.drawCards(1);
+            table.addCards(cards);
+        } else {
+            for (let i=1; i<table.players.length; i++) {
+                table.players[i].revealCards();
+            }
+            await sleep(3000);
+            var winner = table.calculateWinner();
+            handleWinner(winner, "hand");
+        }
+        if (round < 5) {
+            await logGame("Round: " + round);
+            await logGame(`It is ${player1.name}'s turn. How much will you bet or will you fold`);
+        }
     } else {
-        var winner = table.calculateWinner();
-        winner.money += table.pot;
-        document.getElementById('win-menu-background').style.display = "flex";
-        document.getElementById('winner-line').textContent = winner.name + " won with a " + winner.getHand() + "!";
-        document.getElementById('win-amount').textContent = winner.name + " won $" + table.pot; 
-        //alert(winner.name + " won with a " + winner.getHand());
+        await logGame(`It is ${player1.name}'s turn. The round bet was raised to ${table.roundBet}, will you call, raise, or fold?`);
+        document.getElementById("p0-amount").value = table.roundBet - player1.roundBet;
     }
-    if (round < 5) {
-        logGame("Round: " + round);
-        logGame(`It is ${player1.name}'s turn. How much will you bet or will you fold`);
-    }
+    console.log("Table Round Bet: " + table.roundBet);
+    console.log("Player Round Bet: " + table.players[0].roundBet);
+    console.log("CPU Round Bet: " + table.players[1].roundBet);
+    document.getElementById("bet").disabled = false;
+    document.getElementById("fold").disabled = false;
+
 }
 
 function p0Fold() {
-    player1.isFolded = true;
+    console.log("p0 fold");
+    player1.fold();
     var winner = player2;
-    winner.money += table.pot;
-    document.getElementById('win-menu-background').style.display = "flex";
-    document.getElementById('winner-line').textContent = winner.name + " won because all other players folded!";
-    document.getElementById('win-amount').textContent = winner.name + " won $" + table.pot;
-
+    handleWinner(winner, "fold")
 }
-function logGame(message) {
+async function logGame(message) {
+    await sleep(1000);
     const newLog = document.createElement("p"); 
     newLog.textContent = message;
     document.getElementById("sec-game-log").appendChild(newLog);
     document.getElementById('sec-game-log').scrollTo(0,100000);
 
 }
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
 function changeButtonText() {
     console.log("Round Bet: " + table.roundBet);
     console.log("Amount Bet: " + document.getElementById("p0-amount").value);
-    if (Number(document.getElementById("p0-amount").value) === Number(table.roundBet)) {
+    if (Number(document.getElementById("p0-amount").value) === Number(table.roundBet-player1.roundBet)) {
         console.log("Check");
         document.getElementById("bet").value = "Check";
     } else {
@@ -102,8 +151,11 @@ function changeButtonText() {
 }
 
 async function resetRound() {
+    document.getElementById("bet").disabled = true;
+    document.getElementById("fold").disabled = true;
     player1.isFolded = false;
     player2.isFolded = false;
+    cpuRaise = false;
     document.getElementById('win-menu-background').style.display = "none";
     round = 1;
     document.getElementById('sec-game-log').innerHTML = "";
@@ -113,9 +165,21 @@ async function resetRound() {
     player1.setCards([cards[0], cards[1]]);
     player2.setCards([cards[2], cards[3]]);
     table.collectAnte();
-    logGame("Round: " + round);
-    logGame(`It is ${player1.name}'s turn. How much will you bet or will you fold`);
+    await logGame("Round: " + round);
+    await logGame(`It is ${player1.name}'s turn. How much will you bet or will you fold`);
+    document.getElementById("bet").disabled = false;
+    document.getElementById("fold").disabled = false;
+}
 
+function handleWinner(winner, winType) {
+    winner.money += table.pot;
+    document.getElementById('win-menu-background').style.display = "flex";
+    if (winType == "hand") {
+        document.getElementById('winner-line').textContent = winner.name + " won with a " + winner.getHand() + "!";
+    } else if (winType == "fold") {
+        document.getElementById('winner-line').textContent = winner.name + " won because all other players folded!";
+    }
+    document.getElementById('win-amount').textContent = winner.name + " won $" + table.pot;
 }
 
 
